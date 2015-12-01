0d7f75af6f2354bb54722fc785e1f0523e56aba2
Saki Uwagawa <uwagawas@uw.edu>
2015-11-30 21:58:33 -0800

    Basics

---
+++ public/js/info343.js
@@ -0,0 +1,37 @@
+var mainApp = angular.module('mainApp', ['ui.router']);
+
+mainApp.config(function($stateProvider) {
+	$stateProvider.state('home', {
+		url: '/',
+		templateUrl: '../pages/home.html',
+		controller: 'homeCtrl'
+	})
+	.state('syllabus', {
+		url: '/syllabus',
+		templateUrl: '../pages/syllabus.html',
+		controller: 'syllabusCtrl'
+	})
+	.state('calendar', {
+		url: '/calendar',
+		templateUrl: '../pages/calendar.html',
+		controller: 'calendarCtrl'
+	})
+	.state('challenges', {
+		url: '/challenges',
+		templateUrl: '../pages/challenges.html',
+		controller: 'challengesCtrl'
+	})
+});
+
+mainApp.controller('homeCtrl', function($scope) {
+
+})
+.controller('syllabusCtrl', function($scope) {
+
+})
+.controller('calendarCtrl', function($scope) {
+
+})
+.controller('challengesCtrl', function($scope) {
+
+});
