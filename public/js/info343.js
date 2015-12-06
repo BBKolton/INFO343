972a716b21ea6b8ce7579b03f0439075c8264cf0
ying <whydang@uw.edu>
2015-12-06 02:49:22 -0800

    Fixed Calendar

--- public/js/info343.js
+++ public/js/info343.js
@@ -145,8 +145,22 @@ mainApp.controller('homeCtrl', function($scope, $http) {
 
 function calendarFeature(list) {
 	var parentCalendar = $('.calendar').fullCalendar(
-		
-	)
+		{
+        // put your options and callbacks here
+        events: [
+	        {
+	            title:  'Testing; presentations',
+	            start:  '2015-12-03T08:30:00',
+	            allDay: false
+	        }
+        	// other events here...
+        	// will likely be reading from a json file or database to input values in here
+        	// will require some function to reduce redundancy as well
+    	],
+    	timeFormat: 'h(:mm)'
+    	}	
+	);
+
 
     return populateEvent(list, parentCalendar);
 }
